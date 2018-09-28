f584b65c00ff3e755ecac8e753c83b2f7eb35e1b
Hédi <hedi.sassi@epfl.ch>
2018-09-28 19:09:58 +0200

    commit before merge with f1-login

--- app/src/androidTest/java/ch/epfl/sweng/swengproject/RegistrationInstrumentedTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/swengproject/RegistrationInstrumentedTest.java
@@ -1,11 +1,20 @@
 package ch.epfl.sweng.swengproject;
 
-import android.support.test.rule.ActivityTestRule;
+import android.support.annotation.NonNull;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -14,20 +23,47 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertEquals;
+
+import android.support.test.rule.ActivityTestRule;
+
 
 @RunWith(AndroidJUnit4.class)
 public class RegistrationInstrumentedTest {
 
 
+
     @Rule
-    public final ActivityTestRule<RegistrationActivity> mActivityRule = new ActivityTestRule<>(RegistrationActivity.class);
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
 
     @Test
-    public void testCanGreetUsers() {
+    public void testUserCanRegister() throws InterruptedException {
+
+        String mail = "exemple"+Math.random()+"@hotmail.com";
+        String pswd = "exemple"+Math.random();
+
         onView(withId(R.id.register_btn)).perform(click());
-        onView(withId(R.id.password)).perform(typeText("exemplepassword")).perform(closeSoftKeyboard());
-        onView(withId(R.id.email)).perform(typeText("exemple@epfl.ch")).perform(closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText(pswd)).perform(closeSoftKeyboard());
+        onView(withId(R.id.email)).perform(typeText(mail)).perform(closeSoftKeyboard());
         onView(withId(R.id.button)).perform(click());
+
+        FirebaseAuth testAuth = FirebaseAuth.getInstance();
+
+       /* testAuth.signInWithEmailAndPassword(mail, pswd)
+                .addOnCompleteListener(mActivityRule.getActivity(), new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        assertEquals(task.isSuccessful(),true);
+
+                    }
+                });
+*/
+
+     
+
 
 
     }
