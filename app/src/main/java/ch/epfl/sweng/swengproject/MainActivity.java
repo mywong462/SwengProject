df43e243c2105eeac8e0ab318adcf35be81ccee8
Claire <claire.capelo@hotmail.com>
2018-09-28 15:02:17 +0200

    Started creating login activity - issue: no verification email received

--- app/src/main/java/ch/epfl/sweng/swengproject/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/swengproject/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button register_btn;
+    private Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         register_btn = findViewById(R.id.register_btn);
+        login_btn = findViewById(R.id.login_btn);
 
         register_btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
             }
         });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+
+
+        // Use this to check if the email has been confirmed: FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()
+
+
 
     }
 
