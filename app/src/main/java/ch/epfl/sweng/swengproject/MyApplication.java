e8aa600507dc555eb3b8d605bf3ac0224795ad0f
LaFrimousse <kaeser.jonathan@gmail.com>
2018-10-08 12:52:48 +0200

    Imported the files required to make a user persist using the Room API

---
+++ app/src/main/java/ch/epfl/sweng/swengproject/MyApplication.java
@@ -0,0 +1,29 @@
+package ch.epfl.sweng.swengproject;
+
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    /**
+     * Base class for maintaining global application state. You can provide
+     * your own implementation by creating a subclass and specifying the
+     * fully-qualified name of this subclass as the "android:name"
+     * attribute in your AndroidManifest.xml's <application> tag.
+     * The Application class, or your subclass of the Application class,
+     * is instantiated before any other class when the process for your
+     * application/package is created.
+     * https://developer.android.com/reference/android/app/Application
+     */
+
+    private static Context context;
+
+    public void onCreate() {
+        super.onCreate();
+        MyApplication.context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return MyApplication.context;
+    }
+}
---
+++ app/src/main/java/ch/epfl/sweng/swengproject/storage/db/User.java
@@ -0,0 +1,126 @@
+package ch.epfl.sweng.swengproject.storage.db;
+
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.util.Date;
+
+import ch.epfl.sweng.swengproject.storage.filesystem.BitMapStorage;
+import ch.epfl.sweng.swengproject.storage.filesystem.Generic;
+
+
+@Entity(indices = {@Index(value = {"email"}, unique = true)})
+public class User {
+
+    private static String PICTURES_FOLDER = "user_pictures";
+
+    @PrimaryKey
+    private long id = -1;
+    private String firstName = null;
+    private String lastName = null;
+    private String email = null;
+    private Date inscriptionDate = null;
+
+
+
+    @Ignore
+    private Bitmap picture = null;
+
+    /*public User(long id, String firstName, String lastName, String email, Date inscriptionDate, Bitmap picture){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.inscriptionDate = inscriptionDate;
+        this.picture = picture;
+    }*/
+
+    private String pathToPicture(){
+        return PICTURES_FOLDER + File.pathSeparator + id + ".jpeg";
+    }
+
+    /**
+     * Save the picture of the user in the filesystem
+     * @return true if the picture can have been saved, else false
+     */
+    boolean savePicture(){
+        if(picture != null){
+            return BitMapStorage.saveImage(picture, pathToPicture());
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Delete the picture of a user in the file system
+     * @return true if the picture doesn't exist in the file system at
+     *       the end of the invocation of this method, else false
+     */
+    boolean deletePicture(){
+        return Generic.deleteFile(pathToPicture());
+    }
+
+    /**
+     *
+     * @return true if the folder doesn't exist in the file system at
+     *  the end of the invocation of this method, else false
+     */
+    static boolean deletePictureFolder(){
+        return Generic.deleteFolder(PICTURES_FOLDER);
+    }
+
+    //-----------SETTERS----------
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setInscriptionDate(Date inscriptionDate) {
+        this.inscriptionDate = inscriptionDate;
+    }
+    public void setPicture(Bitmap picture) {
+        this.picture = picture;
+    }
+
+    //-----------GETTERS----------
+    public long id(){
+        return id;
+    }
+    public String firstName(){
+        return firstName;
+    }
+    public String lastName(){
+        return lastName;
+    }
+    public String email(){
+        return email;
+    }
+    public Date inscriptionDate(){
+        return inscriptionDate;
+    }
+
+    /**
+     *
+     * @return the picture of the user, if it is not in the memory,
+     * it tries to load it from the disk
+     */
+    public Bitmap picture(){
+        if(picture == null){
+            picture = BitMapStorage.getImage(pathToPicture());
+        }
+        return picture;
+    }
+
+}
