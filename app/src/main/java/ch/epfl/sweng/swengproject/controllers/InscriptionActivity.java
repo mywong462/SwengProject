570d38d5de24bffb81a1cff8fa9d3fd9a573dadf
LaFrimousse <kaeser.jonathan@gmail.com>
2018-10-09 17:04:50 +0200

    starting the new layout for the registration

---
+++ app/src/main/java/ch/epfl/sweng/swengproject/controllers/InscriptionActivity.java
@@ -0,0 +1,16 @@
+package ch.epfl.sweng.swengproject.controllers;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import ch.epfl.sweng.swengproject.R;
+
+public class InscriptionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+    }
+
+}
