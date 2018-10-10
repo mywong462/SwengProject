df48588e3028a4ff94ec14ba107a380de282e82a
LaFrimousse <kaeser.jonathan@gmail.com>
2018-10-10 22:00:25 +0200

    Validator for the inscription done

--- app/src/main/java/ch/epfl/sweng/swengproject/controllers/InscriptionActivity.java
+++ app/src/main/java/ch/epfl/sweng/swengproject/controllers/InscriptionActivity.java
@@ -1,10 +1,12 @@
 package ch.epfl.sweng.swengproject.controllers;
 
+import android.app.Application;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -12,12 +14,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import ch.epfl.sweng.swengproject.MyApplication;
 import ch.epfl.sweng.swengproject.R;
 import ch.epfl.sweng.swengproject.RegistrationActivity;
+import ch.epfl.sweng.swengproject.helpers.inputvalidation.InscriptionValidator;
 import ch.epfl.sweng.swengproject.storage.db.AppDatabase;
 import ch.epfl.sweng.swengproject.storage.db.User;
 import ch.epfl.sweng.swengproject.storage.db.UserDao;
@@ -69,6 +74,7 @@ public class InscriptionActivity extends Activity {
     }
 
     private void pickImage(){
+        //https://www.youtube.com/watch?v=Mm1dMWZWQ6w
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_PICK);
@@ -98,22 +104,52 @@ public class InscriptionActivity extends Activity {
     }
 
     private void register(){
-        User me = new User();
+
+        if(InscriptionValidator.fieldsAreValid(emailEditText.getText().toString(), pswEditText.getText().toString(),
+                firstNameEditText.getText().toString(), lastNameEditText.getText().toString())){
+            Toast.makeText(this, "all is OK " +emailEditText.toString(), Toast.LENGTH_SHORT).show();
+
+        }
+
+        /*https://developer.android.com/reference/android/os/AsyncTask
+        AsyncTask is designed to be a helper class around Thread
+         and Handler and does not constitute a generic threading framework.
+          AsyncTasks should ideally be used for short operations
+          (a few seconds at the most.) [...]
+
+          An asynchronous task is defined by a computation that
+          runs on a background thread and whose result is published
+          on the UI thread. An asynchronous task is defined by 3 generic types,
+           called Params, Progress and Result,
+           and 4 steps, called onPreExecute, doInBackground, onProgressUpdate and onPostExecute*/
+
+        /*User me = new User();
         me.setEmail(emailEditText.toString());
         me.setPassword(pswEditText.toString());
         me.setFirstName(firstNameEditText.toString());
         me.setLastName(lastNameEditText.toString());
         Bitmap bm = ((BitmapDrawable)profilePictureButton.getDrawable()).getBitmap();
-        me.setPicture(bm);
-        UserDao userDao = AppDatabase.getInMemoryDatabase(this).userDao();
-        userDao.storeMyOwnProfile(me);
+        me.setPicture(bm);*/
 
-        User meButFetched = userDao.fetchMyOwnProfile();
+        //UserDao userDao = AppDatabase.getInMemoryDatabase(this).userDao();
+        //userDao.storeMyOwnProfile(me);
+
+        /*User meButFetched = userDao.fetchMyOwnProfile();
         emailEditText.setText(meButFetched.email(),TextView.BufferType.EDITABLE);
         pswEditText.setText(meButFetched.password(),TextView.BufferType.EDITABLE);
         firstNameEditText.setText(meButFetched.firstName(),TextView.BufferType.EDITABLE);
         lastNameEditText.setText(meButFetched.lastName(),TextView.BufferType.EDITABLE);
-        profilePictureButton.setImageBitmap(meButFetched.picture());
+        profilePictureButton.setImageBitmap(meButFetched.picture());*/
+
+         /*new  AsyncTask<User, Void, Void>() {
+            @Override
+            protected Void doInBackground(User... users) {
+                User u = users[0];
+                UserDao userDao = AppDatabase.getInMemoryDatabase(MyApplication.getAppContext()).userDao();
+                userDao.storeMyOwnProfile(u);
+                return null;
+            }
+        }.execute(me);*/
 
 
     }
---
+++ app/src/main/java/ch/epfl/sweng/swengproject/helpers/inputvalidation/InscriptionValidator.java
@@ -0,0 +1,34 @@
+package ch.epfl.sweng.swengproject.helpers.inputvalidation;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import ch.epfl.sweng.swengproject.MyApplication;
+
+public class InscriptionValidator {
+    private InscriptionValidator(){}
+
+    public static boolean fieldsAreValid(String email, String password, String firstName, String lastName){
+
+        if(email == null || email.length() == 0){
+            Toast.makeText(MyApplication.getAppContext(), "An email address is mandatory", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(email.length() > 50){
+            Toast.makeText(MyApplication.getAppContext(), "An email address cannot be longer than 50 characters", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(password == null || password.length() < 6){
+            Toast.makeText(MyApplication.getAppContext(), "A password of length 6 minimum is required", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(password.length() > 50){
+            Toast.makeText(MyApplication.getAppContext(), "Your password cannot be longer than 50 characters", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(firstName != null && firstName.length() > 50){
+            Toast.makeText(MyApplication.getAppContext(), "Your first name cannot be longer than 50 characters", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(lastName != null && lastName.length() > 50){
+            Toast.makeText(MyApplication.getAppContext(), "Your last name cannot be longer than 50 characters", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+}
