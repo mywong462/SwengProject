4dabc23109f485b03409bb97ba1e3b73d0b77a6f
Simon Perriard <simon.perriard@epfl.ch>
2018-10-14 20:37:33 +0200

    CodeClimate changes

--- app/src/androidTest/java/ch/epfl/sweng/swengproject/MapsInstrumentedTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/swengproject/MapsInstrumentedTest.java
@@ -1,40 +1,21 @@
 package ch.epfl.sweng.swengproject;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.support.test.espresso.NoMatchingViewException;
-import android.support.test.espresso.core.internal.deps.guava.collect.Iterables;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
-import android.support.test.runner.lifecycle.Stage;
-import android.util.Log;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.content.Context.CONTEXT_IGNORE_SECURITY;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
 import static android.support.test.espresso.action.ViewActions.typeText;
+
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.fail;
-import static ch.epfl.sweng.swengproject.MainActivity.currentLocation;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 @RunWith(AndroidJUnit4.class)
