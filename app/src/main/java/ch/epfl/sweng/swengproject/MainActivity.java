555eeee4606a688d3849931c867fa5472f89307d
Hédi <hedi.sassi@epfl.ch>
2018-10-21 20:23:43 +0200

    purged timer

--- app/src/main/java/ch/epfl/sweng/swengproject/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/swengproject/MainActivity.java
@@ -57,8 +57,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent m) {
 
                 timer.cancel();
-               // timer.purge();
-                timer = null;
+                timer.purge();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.transition.slide_in_left, R.transition.slide_out_left);
