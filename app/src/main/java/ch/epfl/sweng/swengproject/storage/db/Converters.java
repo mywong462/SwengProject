b64d0b5284c965639588eb6196a16e192bc5fd5f
LaFrimousse <kaeser.jonathan@gmail.com>
2018-10-24 10:50:10 +0200

    updated the files in the db package

--- app/src/main/java/ch/epfl/sweng/swengproject/storage/db/Converters.java
+++ app/src/main/java/ch/epfl/sweng/swengproject/storage/db/Converters.java
@@ -5,10 +5,9 @@ import android.arch.persistence.room.TypeConverter;
 
 import java.util.Date;
 
-public class Converters {
+class Converters {
     @TypeConverter
     public static Date fromTimestamp(Long value) {
-
         return value == null ? null : new Date(value);
     }
 
